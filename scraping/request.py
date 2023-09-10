5994398f8ad2704e19d56471d8af808a8e613ab2
Lautarocm <lcardenasmancini@gmail.com>
2023-09-10 04:30:28 -0300

    eliminación codigo viejo e integracion con backend

--- scraping/request.py
+++ scraping/request.py
@@ -4,30 +4,12 @@ import aiohttp
 from headers import headers
 import json
 import uuid
-
+import sys
 
 
 def read_json(filename):
     with open(filename, "r", encoding="utf-8") as json_file:
         return json.load(json_file)
-    
-
-
-def save_json(filename, dict):
-    with open(filename, "w", encoding="utf-8") as json_file:
-        json.dump(dict, json_file)
-
-
-
-def input_origin():
-    return input("Origen: ")
-
-
-
-def set_destination():
-    json_destinations = read_json("./scraping/destinations.json")[0]["destinos"]
-    return [destination["id"].upper() for destination in json_destinations]
-
 
 
 def set_date():
@@ -36,7 +18,6 @@ def set_date():
     year = today.year
     month = today.month
     day = today.day
-
     if day<=16:
         dates_list.append(f"{year:04}{month:02}16")
     else:
@@ -47,14 +28,11 @@ def set_date():
         if month>12:
             month = today.month + i - 12
             year += 1
-
         dates_list.append(f"{year:04}{month:02}16")
-    
     return dates_list
 
 
-
-def set_url(origin, destinations, dates):
+def set_url(origin, destination, dates):
     urls_list = []
     endpoint = "https://api.aerolineas.com.ar/v1/flights/offers"
     adults = "1"
@@ -64,104 +42,77 @@ def set_url(origin, destinations, dates):
     cabin_class = "Economy"
     flight_type = "ONE_WAY"
     for date in dates:
-                #Este bloque busca en un solo destino para evitar baneo de ip por exceso de peticiones, por eso uso {destinations[3]}, para buscar en un solo destino del JSON
-        params = f"?adt={adults}&inf={babies}&chd={children}&flexDates={flex_dates}&cabinClass={cabin_class}&flightType={flight_type}&leg={origin}-{destinations[3]}-{date}"
+        params = f"?adt={adults}&inf={babies}&chd={children}&flexDates={flex_dates}&cabinClass={cabin_class}&flightType={flight_type}&leg={origin}-{destination}-{date}"
         urls_list.append(endpoint + params)
-
-            # Este bloque de codigo busca en multiples destinos. IMPORTANTE! ROTAR IPs PARA EVITAR BANEO
-        # for destination in destinations:
-        #     params = f"?adt={adults}&inf={babies}&chd={children}&flexDates={flex_dates}&cabinClass={cabin_class}&flightType={flight_type}&leg={origin}-{destination}-{date}"
-        #     urls_list.append(endpoint + params)
-
     return urls_list
 
 
-
+            #el código comentado en esta función es para que no salga en la salida del child_proccess en Node
 async def fetch_url(session, url):
     try:
         async with session.get(url, headers=headers) as response:
             if response.status == 200:
-                print(response.status)
+                # print(response.status)
                 return await response.json()
-            else:
-                print(f"error {response.status}")
+            # else:
+            #     print(f"error {response.status}") 
     except aiohttp.ClientError as e:
-        print(f"Error: {e}")
+        # print(f"Error: {e}")
         return None
-    
-
+   
 
 def get_city_name(city_code):
-    json_destinations = read_json("./scraping/destinations.json")[0]["destinos"]
+    json_destinations = read_json("C:/Users/Usuario/Documents/proyectos/aerolineas-api/data/airports.json")
     for destination in json_destinations:
-        if city_code.lower() == destination["id"].lower():
-           return destination["cityName"]
+        if city_code.lower() == destination["iataCode"].lower():
+           return destination["city"]["name"]
 
 
+async def clean_offers(responses):
+    filtered_empty_results = [offer["calendarOffers"]["0"] for offer in responses if len(offer["calendarOffers"]) > 0]
+    filtered_offers = []
+    for month_offers in filtered_empty_results:
+        for offer in month_offers:
+            if offer["offerDetails"] != None:
+                filtered_offers.append(offer)
+    return filtered_offers
 
-async def clean_json(responses):
+
+def create_json_offers(cleaned_offers):
     offers_list = []
-    
-    for res in responses:
-        empty_results = False
-        messages = res["searchMetadata"]["infoMessages"]
-        
-        for msg in messages:
-            if "gds.flights.info.emptyResults" in msg:
-                empty_results = True
-
-        if not empty_results:
-            offers = res["calendarOffers"]["0"]
-            
-            for offer in offers:
-                offer_exists = offer["offerDetails"] != None
-                if offer_exists:
-                    flight_data = offer["leg"]["segments"][0]
-                    offer_data = offer["offerDetails"]
-
-                    offer_dict = {
-                        "id": f"{uuid.uuid4()}",
-                        "flightNumber": f'{flight_data["flightNumber"]}',
-                        "origin": flight_data["origin"],
-                        "destination": flight_data["destination"],
-                        "departure": flight_data["departure"],
-                        "arrival": flight_data["arrival"],
-                        "duration": flight_data["duration"],
-                        "cabinClass": offer_data["cabinClass"],
-                        "availability": offer_data["seatAvailability"]["seats"],
-                        "price": offer_data["fare"]["total"],
-                        "originCity": get_city_name(flight_data["origin"]),
-                        "destinationCity": get_city_name(flight_data["destination"]),
-                    }
-                    offers_list.append(offer_dict)
-                    
+    for offer in cleaned_offers:
+        offer_data = offer["offerDetails"]
+        segments = offer["leg"]["segments"]
+        offer_dict = {
+                "id": f"{uuid.uuid4()}",
+                "flightsNumbers": [f'{segment["flightNumber"]}' for segment in segments],
+                "origin": segments[0]["origin"],
+                "destination": segments[len(segments)-1]["destination"],
+                "departures": [segment["departure"] for segment in segments],
+                "arrivals": [segment["arrival"]for segment in segments],
+                "duration": offer["leg"]["totalDuration"],
+                "cabinClass": offer_data["cabinClass"],
+                "availability": offer_data["seatAvailability"]["seats"],
+                "price": offer_data["fare"]["total"],
+                "originCity": get_city_name(segments[0]["origin"]),
+                "destinationCity": get_city_name(segments[len(segments)-1]["destination"]),
+                "connections": offer["leg"]["stops"],
+                "connectionsAirports": [segment["destination"] for i, segment in enumerate(segments) if i < len(segments)-1]
+            }
+        offers_list.append(offer_dict)
     return offers_list
 
 
-
-def sort_offers(offers):
-    return sorted(offers, key=lambda d: d['precio'])
-
-
-
-def save_offers(sorted_offers):
-    offers = open("./scraping/offers2.txt", 'w', encoding="utf-8")
-    offers.write(str(sorted_offers).replace("}, ", "}\n").replace("[", "").replace("]", "").replace("{", "").replace("}", "").replace("'", "").replace("precio: ", "precio: $"))
-
-
-
-async def main():
-    origin = input_origin()
-    destinations = set_destination()
+async def main(origin, destination):
     dates = set_date()
-    urls = set_url(origin, destinations, dates)
-
+    urls = set_url(origin, destination, dates)
     async with aiohttp.ClientSession() as session:
         tasks = [fetch_url(session, url) for url in urls]
         responses = await asyncio.gather(*tasks)
+    cleaned_offers = await clean_offers(responses)
+    final_offers = create_json_offers(cleaned_offers)
+    return final_offers
 
-    offers = await clean_json(responses)
-    save_json("./data/offers.json", offers)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+final_offers = asyncio.run(main(sys.argv[1], sys.argv[2]))
+print(json.dumps(final_offers))
