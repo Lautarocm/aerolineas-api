5c496e0e040b35730d1376b516f014a23b4c05f9
Lautarocm <lcardenasmancini@gmail.com>
2023-09-11 02:07:55 -0300

    cambio de metodo para obtener token

--- scraping/get_token.py
+++ scraping/get_token.py
@@ -1,24 +1,20 @@
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from seleniumwire import webdriver
 import os
-from utilities import get_script_directory as dir
+from utilities import get_script_directory as directory
 
 options = webdriver.ChromeOptions()
-options.add_argument('--proxy-server="direct://"')
-options.add_argument('--proxy-bypass-list=*')
 options.add_argument('--headless')
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
-driver.get("https://www.aerolineas.com.ar/")
+driver.get('https://www.aerolineas.com.ar/')
 
-url = "https://api.aerolineas.com.ar/v1/catalog/cabins"
+token = driver.execute_script('return window.__ACCESS_TOKEN__;')
 
-current_dir = dir(__file__)
+current_dir = directory(__file__)
 token_dir = os.path.join(current_dir, "token.txt")
 
-for request in driver.requests:
-    if url in request.url:
-        with open(token_dir, "w") as file:
-            file.write(request.headers["authorization"])
+with open(token_dir, "w") as file:
+    file.write(f"Bearer {token}")
